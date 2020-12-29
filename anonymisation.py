c39f09a9fd99f79d1a53c6077c6755e90de39765
Ning107 <ningzgre@gmail.com>
2020-12-29 22:39:53 +0100

    ajouter anonymisation version 1

---
+++ anonymisation.py
@@ -0,0 +1,90 @@
+import spacy
+from spacy.matcher import Matcher
+import os
+
+path = "Corpus"
+files = os.listdir(path)
+
+# effacer le fichier ".DS_Store"
+for file in files:
+    if file.startswith('.') and os.path.isfile(os.path.join(path,file)):
+        files.remove(file)
+
+# parcourire tous les textes dans le dossier "Corpus"
+for file in files:
+    if not os.path.isdir(file):
+        f = open(path + "/" + file);
+        iter_f = iter(f)
+        text = ""
+        for line in iter_f:
+            text += line # la variable "text" contient le contenu de chaque fichier
+
+#text = "Téléphone : 04-77-30-48-44 ou 06-87-58-12-62 ou 07.70.50.22.22 ou 06 72 34 65 62 ou 0632121854 . Et l' adresse mail de Léa est ningzgre@gmial.com"
+
+        processeur = spacy.load("fr")
+        contenu = processeur(text)
+
+        # initialiser Matcher
+        matcher = Matcher(processeur.vocab)
+
+        ########## Numero ##########
+        # matche les numero de format 04-77-30-48-44 ou 06 72 34 65 62
+        patternTel1 = [
+                    {"SHAPE":'dd'},
+                    {'ORTH': '-', 'OP': '?'},
+                    {"SHAPE":'dd'},
+                    {'ORTH': '-', 'OP': '?'},
+                    {"SHAPE": 'dd'},
+                    {'ORTH': '-', 'OP': '?'},
+                    {"SHAPE": 'dd'},
+                    {'ORTH': '-', 'OP': '?'},
+                    {"IS_DIGIT":True}]
+        # matche les numero de format 0611223344
+        patternTel2 = [{"TEXT":{"REGEX": "[0-9]{10}"}}]
+        # matche les numero de format 06.11.22.33.44
+        patternTel3 = [{"TEXT":{"REGEX": "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"}}]
+
+        # ajouter le paterne
+        matcher.add("TEL_PATTERN",None,patternTel1,patternTel2,patternTel3)
+
+        # trouver tous les matches dans le texte
+        matches = matcher(contenu)
+
+        # anonymiser les numeros trouves
+        tels = []
+        for match_id, start, end in matches:
+            span = contenu[start:end]
+            tels.append(span.text)
+
+        for tel in tels:
+            text = text.replace(tel,'[Tel]')
+
+        ########## Mail ##########
+        # match le contenu de email
+        patternEmail = [{"TEXT": {"REGEX": "[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+"}}]
+        matcher.add("Email", None, patternEmail)
+        # trouver tous les matches dans le texte
+        matches = matcher(contenu)
+        # anonymiser les mails trouves
+        mails = []
+        for match_id, start, end in matches:
+            span = contenu[start:end]
+            mails.append(span.text)
+
+        for mail in mails:
+            text = text.replace(mail, '[Mail]')
+
+        ########## entités nommées ##########
+
+        # Reconnaitre les entités nommées
+        Enti = []
+        for ent in contenu.ents:
+            Enti.append((ent.text,ent.label_))
+
+        # anaonymiser les entités nommées par leurs étiquettes
+        for elt in Enti:
+            print(elt)
+            text = text.replace(elt[0],"["+elt[1]+"]")
+
+        print(text)
+
--- scraper.py
+++ scraper.py
@@ -8,11 +8,14 @@ url = "https://www.leboncoin.fr/_immobilier_/offres"    # récupère l'url de la
 r = session.get(url)
 container = r.html.find(".styles_mainListing__PjWbm", first=True)   # colonne avec toutes les annonces de la page
 a = container.absolute_links    # tableau avec les liens des annonces
-# print(container.text)
+#print(container.text)
 
 for lien in a :
     r2 = session.get(lien)  # récupère le lien de chaque annonce
     summary = r2.html.find(".styles_Summary__Bcg1X", first=True)    # résumé de l'annonce (titre, prix, date de publication)
     descr = r2.html.find("._1fFkI", first=True)     # description de l'annonce
-    # print(summary.text)
-    # print(descr.text)
+    name = lien[len(lien)-14:len(lien)-4]
+    print(summary.text)
+    print(descr.text)
+    with open("Corpus/"+name+"txt","w") as fichier:
+        fichier.write(summary.text+"\n"+descr.text)
